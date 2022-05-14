dde884d320b94255821f778b07cc6736ff81f5d5
Don <donavan.olsen@gmail.com>
2022-05-14 08:07:25 -0700

    Refactoring
    Created static file to hold logic for CardSet Table access

--- Data/Contexts/SqliteContext.cs
+++ Data/Contexts/SqliteContext.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using CardShow.Data.Models;
 using CardShow.Data.SqliteSchema;
 using System.Data;
+using CardShow.Data.Contexts.Tables;
 
 namespace CardShow.Data.Contexts
 {
@@ -21,103 +22,28 @@ namespace CardShow.Data.Contexts
         {
             this.conn = conn;
             conn.Open();
-            Sets = TryReadSets();
+            Sets = CardSetTable.TryReadSets(conn);
         }
 
         public async Task<int> CreateSet(_CardSet set)
         {
-            using (var cmd = conn.CreateCommand())
-            {
-                cmd.CommandText = CreateRow.Set;
-                AddParameter(cmd, "@year", set.Year);
-                AddParameter(cmd, "@name", set.Name);
-                long setId = (long)cmd.ExecuteScalar();
-                set.Id = (int)setId;
-            }
+            var id = await CardSetTable.Create(conn, set);
 
             RefreshSetsList();
-            return await Task.FromResult(set.Id);
+            return await Task.FromResult(id);
         }
 
         public async Task DeleteSet(int id)
         {
-            var cmd = conn.CreateCommand();
-            cmd.CommandText = DeleteRow.Set;
-            AddParameter(cmd, "@id", id);
-            cmd.ExecuteNonQuery();
-
+            await CardSetTable.DeleteSet(conn, id);
             RefreshSetsList();
             await Task.CompletedTask;
-        }
-
-        private IEnumerable<_CardSet> TryReadSets()
-        {
-            try
-            {
-                return ReadSets();
-            }
-            catch
-            {
-                SqliteDbPlant.CreateTable_CardSets(conn);
-                SqliteDbPlant.SeedData_CardSets(conn);
-                return ReadSets();
-            }
-        }
-
-        private IEnumerable<_CardSet> ReadSets()
-        {
-            var sets = new List<_CardSet>();
-
-            using (var command = conn.CreateCommand())
-            {
-                command.CommandText = ReadTable.CardSets;
-                using (var reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        sets.Add(new _CardSet()
-                        {
-                            Id = reader.GetInt32(0),
-                            Year = reader.GetInt32(1),
-                            Name = reader.GetString(2),
-                        });
-                    }
-                };
-            }
-
-            return sets;
         }
 
         private void RefreshSetsList()
         {
             Sets = new List<_CardSet>();
-            Sets = ReadSets();
-        }
-
-        private void AddParameter(
-            DbCommand cmd,
-            string name,
-            object value)
-        {
-            var p = cmd.CreateParameter();
-            if (value == null)
-            {
-                throw new ArgumentNullException("Parameter value cannot be null");
-            }
-
-            var type = value.GetType();
-            if (type == typeof(int))
-                p.DbType = DbType.Int32;
-            else if (type == typeof(string))
-                p.DbType = DbType.String;
-            else
-                throw new ArgumentException(
-                    $"Unrecognized Type: {type}");
-
-            p.Direction = ParameterDirection.Input;
-            p.ParameterName = name;
-            p.Value = value;
-            cmd.Parameters.Add(p);
+            Sets = CardSetTable.ReadSets(conn);
         }
 
         public void Dispose()
---
+++ Data/Contexts/Tables/CardSetTable.cs
@@ -0,0 +1,108 @@
+﻿using CardShow.Data.Models;
+using CardShow.Data.SqliteSchema;
+using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardShow.Data.Contexts.Tables
+{
+    internal static class CardSetTable
+    {
+        internal static async Task<int> Create(
+            SqliteConnection conn, _CardSet set)
+        {
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = CreateRow.Set;
+                AddParameter(cmd, "@year", set.Year);
+                AddParameter(cmd, "@name", set.Name);
+                long setId = (long)cmd.ExecuteScalar();
+                set.Id = (int)setId;
+            }
+
+            return await Task.FromResult(set.Id);
+        }
+
+        internal static async Task DeleteSet(
+            SqliteConnection conn, int id)
+        {
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = DeleteRow.Set;
+            AddParameter(cmd, "@id", id);
+            cmd.ExecuteNonQuery();
+
+            await Task.CompletedTask;
+        }
+
+        internal static  IEnumerable<_CardSet> TryReadSets(
+            SqliteConnection conn)
+        {
+            try
+            {
+                return ReadSets(conn);
+            }
+            catch
+            {
+                SqliteDbPlant.CreateTable_CardSets(conn);
+                SqliteDbPlant.SeedData_CardSets(conn);
+                return ReadSets(conn);
+            }
+        }
+
+        internal static IEnumerable<_CardSet> ReadSets(
+            SqliteConnection conn)
+        {
+            var sets = new List<_CardSet>();
+
+            using (var command = conn.CreateCommand())
+            {
+                command.CommandText = ReadTable.CardSets;
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        sets.Add(new _CardSet()
+                        {
+                            Id = reader.GetInt32(0),
+                            Year = reader.GetInt32(1),
+                            Name = reader.GetString(2),
+                        });
+                    }
+                };
+            }
+
+            return sets;
+        }
+
+        private static void AddParameter(
+            DbCommand cmd,
+            string name,
+            object value)
+        {
+            var p = cmd.CreateParameter();
+            if (value == null)
+            {
+                throw new ArgumentNullException("Parameter value cannot be null");
+            }
+
+            var type = value.GetType();
+            if (type == typeof(int))
+                p.DbType = DbType.Int32;
+            else if (type == typeof(string))
+                p.DbType = DbType.String;
+            else
+                throw new ArgumentException(
+                    $"Unrecognized Type: {type}");
+
+            p.Direction = ParameterDirection.Input;
+            p.ParameterName = name;
+            p.Value = value;
+            cmd.Parameters.Add(p);
+        }
+    }
+}
