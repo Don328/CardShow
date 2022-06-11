1ac48f0b6e61a43e33571615b094f5b3df1566b0
Don <donavan.olsen@gmail.com>
2022-06-11 09:47:37 -0700

    Refactoring
    Changed 'CardViewer' component to 'CollectionView'
    Added CardShow.Web.Components to '_ViewImports.html' and shortened call in index page

---
+++ Web/Components/SubComponents/Assessments/AssessmentInfo.razor.cs
@@ -0,0 +1,11 @@
+﻿using CardShow.Shared.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace CardShow.Web.Components.SubComponents.Assessments
+{
+    public partial class AssessmentInfo: ComponentBase
+    {
+        [Parameter]
+        public Assessment Assessment { get; set; } = new();
+    }
+}
